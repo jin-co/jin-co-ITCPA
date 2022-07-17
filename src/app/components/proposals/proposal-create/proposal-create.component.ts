9cd81233811b7f9bd190298a8efc8ae32a99a8ba
jin-co <lilijk@hanmail.net>
2022-07-17 17:18:11 +0900

    Update

--- src/app/components/proposals/proposal-create/proposal-create.component.ts
+++ src/app/components/proposals/proposal-create/proposal-create.component.ts
@@ -22,7 +22,7 @@ import { Project } from 'src/app/models/project/project';
 export class ProposalCreateComponent implements OnInit {
   contactInfoGroup!: FormGroup;
   projectDetailGroup!: FormGroup;
-  thirdFormGroup!: FormGroup;
+  fileUploadGroup!: FormGroup;
   toppings = new FormControl('');
 
   programs: string[] = ['CP', 'CPA', 'ITCP', 'CDCD', 'All'];
@@ -38,10 +38,10 @@ export class ProposalCreateComponent implements OnInit {
     this.projectDetailGroup = this._formBuilder.group({
       title: ['', Validators.required],
       description: ['', Validators.required],
-      targetProgram: [[], Validators.required],
+      targetProgram: ['', Validators.required],
     });
-    this.thirdFormGroup = this._formBuilder.group({
-      file: ['', Validators.required],
+    this.fileUploadGroup = this._formBuilder.group({
+      file: [null],
     });
   }
 
@@ -56,18 +56,33 @@ export class ProposalCreateComponent implements OnInit {
     }
   }
 
-  newProjectDetail!: Project
+  newProjectDetail!: Project;
   addProjectDetail() {
     if (this.projectDetailGroup.valid) {
       this.newProjectDetail = {
         projectName: this.projectDetailGroup.value.title,
         projectDescription: this.projectDetailGroup.value.description,
         targetProgram: this.projectDetailGroup.value.targetProgram,
-      };      
+      };
 
-      console.log('added 1: ', this.newContactInfo)
-      console.log('added 2: ', this.newProjectDetail)
-      console.log('project: ', this.projectDetailGroup)
+      console.log('added 1: ', this.newContactInfo);
+      console.log('added 2: ', this.newProjectDetail);
+      console.log('project: ', this.projectDetailGroup);
     }
+  }
+
+  preview!: string;
+  //todo -> fix error
+  addFiles(e: Event) {    
+    const file = (e.target as HTMLInputElement).files?.[0];    
+    this.fileUploadGroup.patchValue({ file: file });
+    console.log(file)
+    this.fileUploadGroup.get('file')?.updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.preview = reader.result as string;
+    };
+
+    reader.readAsDataURL(file as Blob);
   }
 }
