d4798297138eb887df018b4c3b2eddb793045b75
jin-co <lilijk@hanmail.net>
2022-07-17 16:32:41 +0900

    Update

--- backend/middlewares/file-check.js
+++ backend/middlewares/file-check.js
@@ -1,23 +1,12 @@
 const multer = require('multer')
-const MIME_TYPE_MAP = {
-  "image/png": "png",
-  "image/jpeg": "jpg",
-  "image/jpg": "jpg",
-}
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP(file.mimetype)
-    let error = new Error('invalid')
-    if(isValid) {
-      error = null
-    }
-    cb(error, "backend/images")
-
+  destination: (req, file, cb) => {    
+    cb(null, "backend/images")
   },
   filename: (req, file, cb) => {
     const name = file.originalname.toLowerCase().split(' ').join('-')
-    const ext = MIME_TYPE_MAP[file.mimetype]
+    const ext = file.mimetype
     cb(null, name + "-" + Date.now() + "." + ext)
   }
 })
