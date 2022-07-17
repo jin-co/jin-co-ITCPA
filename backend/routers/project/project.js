3526a4dca3d7a8c9e8085d559ae22f37ace2db09
jin-co <lilijk@hanmail.net>
2022-07-17 11:06:55 +0900

    Added project service

---
+++ backend/routers/project/project.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../../models/my-db')
+
+
+router.post("", (req, res, next) => {
+  
+})
+
+module.exports = router
