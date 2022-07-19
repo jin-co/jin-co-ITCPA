c0f2da161a26f98ffaada579ff3e28ef57213e20
jin-co <lilijk@hanmail.net>
2022-07-19 20:56:24 +0900

    Update

--- server.js
+++ server.js
@@ -1,9 +1,3 @@
-const express = require('express')
-const set = express()
-set.use(express.static(__dirname + '/dist/itcpa'))
-set.get('/*', (req, res) => {
-  res.sendFile(__dirname + '/dist/itcpa/index.thml')
-})
 const http = require('http')
 const port = process.env.PORT | 3000
 const app = require('./backend/app')
