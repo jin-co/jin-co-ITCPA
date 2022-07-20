e196399c802d27b5a6137c2898fe2c3d98769c35
jin-co <lilijk@hanmail.net>
2022-07-20 17:20:48 +0900

    Update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'itcpa'`, () => {
+  it(`should have as title 'ssibal'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('itcpa');
+    expect(app.title).toEqual('ssibal');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('itcpa app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ssibal app is running!');
   });
 });
